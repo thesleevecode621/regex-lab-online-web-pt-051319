14c74fadfbe4fc36421bbed56c8984ae70e3b686
boruch d <dubywider@gmail.com>
2019-05-13 18:20:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,8 +14,8 @@ text.scan (/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match (/^[a-z].+[\.!?]$/)? true : false
 
+text.match(/^[A-Z].+[\.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
