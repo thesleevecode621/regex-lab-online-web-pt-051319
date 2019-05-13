d43a7751b6e7a005bc69dc9ce1a41a7e158a2581
boruch d <dubywider@gmail.com>
2019-05-13 18:19:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,5 +18,5 @@ text.match (/^[a-z].+[\.!?]$/)? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+phone.match (/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
