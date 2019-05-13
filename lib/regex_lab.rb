a3cfb5a619ca33db2d67b401d2ba0c9056bf65b8
boruch d <dubywider@gmail.com>
2019-05-13 18:19:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,6 +18,6 @@ text.match (/^[a-z].+[\.!?]$/)? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match (/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 
+phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 end
